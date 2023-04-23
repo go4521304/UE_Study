497eafa53bf6267f3b6dc385e5ea971aa2b33eab
Kclient <go4521304@gmail.com>
2023-04-24 01:34:41 +0900

    add1

---
+++ Source/Test/Public/MyPawnMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "MyPawnMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API UMyPawnMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
