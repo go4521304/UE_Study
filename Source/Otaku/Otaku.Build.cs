a9a24eb310f18d60aa9d617443b5d8c018dd87f2
go4521304 <go4521304@gmail.com>
2024-10-10 10:42:29 +0900

    build.cs 변경

--- Source/Otaku/Otaku.Build.cs
+++ Source/Otaku/Otaku.Build.cs
@@ -8,6 +8,6 @@ public class Otaku : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AkAudio", "WwiseSoundEngine" });
 	}
 }
